66aa6dde7f2830d71cdc18b1e7a819a3ef41b3dd
Alex Larioza <contact@alexlarioza.com>
2021-08-13 11:53:05 -0700

    added image plugin to automatically generate thumbnails for artwork nav bar

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,29 @@
+const Image = require("@11ty/eleventy-img");
+
+async function thumbnailShortcode(src, alt) {
+  if (alt === undefined) {
+    // You bet we throw an error on missing alt (alt="" works okay)
+    throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+  }
+
+  let metadata = await Image(src, {
+    widths: [200],
+    formats: ["jpeg"],
+    urlPath: "/thumbnails/",
+    outputDir: "./_site/thumbnails/"
+  });
+
+  let data = metadata.jpeg[metadata.jpeg.length - 1];
+  return `<img src="${data.url}" width="${data.width}" height="${data.height}" alt="${alt}" loading="lazy" decoding="async">`;
+}
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./_tmp/style.css')
 
   eleventyConfig.addPassthroughCopy({ './_tmp/style.css': './style.css' })
   eleventyConfig.addPassthroughCopy({ './src/media/': './media/' })
+
+  eleventyConfig.addLiquidShortcode("thumbnail", thumbnailShortcode);
 
   return {
     dir: {
