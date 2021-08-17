5c106673aaccf64e104519edacae56e3222bbbd2
Alex Larioza <contact@alexlarioza.com>
2021-08-16 20:37:38 -0700

    working on artwork nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,10 @@ module.exports = {
       red: colors.red,
       gray: colors.trueGray
     },
+
+    boxShadow: {
+      DEFAULT: '0 -5px 25px -5px rgba(0, 0, 0, .85), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+    },
   },
 
   variants: {},
