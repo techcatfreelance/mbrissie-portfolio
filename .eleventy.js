f4c150f52b258c013926ce7ac8794443fd33c1a3
Alex Larioza <contact@alexlarioza.com>
2021-08-19 20:33:30 -0700

    added shortcode for artwork to insure size and format for load times/bandwidth

--- .eleventy.js
+++ .eleventy.js
@@ -17,6 +17,23 @@ async function artworkThumbnailShortcode(src, alt) {
   return `<img class="lozad" data-src="${data.url}" width="${data.width}" height="${data.height}" alt="${alt}" loading="lazy" decoding="async">`;
 }
 
+async function artworkImgShortcode(src, alt) {
+  if (alt === undefined) {
+    // You bet we throw an error on missing alt (alt="" works okay)
+    throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+  }
+
+  let metadata = await Image(src, {
+    widths: [1280],
+    formats: ["jpeg"],
+    urlPath: "/artwork/",
+    outputDir: "./_site/artwork/"
+  });
+
+  let data = metadata.jpeg[metadata.jpeg.length - 1];
+  return `<img class="bg-black w-full border-4 border-black lozad" data-src="${data.url}" width="${data.width}" height="${data.height}" alt="${alt}" loading="lazy" decoding="async">`;
+}
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./_tmp/style.css')
 
@@ -24,6 +41,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ './src/media/': './media/' })
 
   eleventyConfig.addLiquidShortcode("artworkThumbnail", artworkThumbnailShortcode);
+  eleventyConfig.addLiquidShortcode("artworkImg", artworkImgShortcode);
 
   return {
     dir: {
