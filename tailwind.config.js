4226727b3a6e199f5a60a336efca466ea4c5a930
Alex Larioza <contact@alexlarioza.com>
2021-08-31 13:26:41 -0700

    added screen breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
   },
 
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+
     fontFamily: {
       sans: [
         'Roboto',
